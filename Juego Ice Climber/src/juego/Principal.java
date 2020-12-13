09c7964928b04ee4a869f497a1ab20bc6110183d
davidaqc <davidqc05@gmail.com>
2020-12-13 11:11:10 -0600

    Subida de algunos recursos necesarios (librerias, imagenes)

--- Juego Ice Climber/src/juego/Principal.java
+++ Juego Ice Climber/src/juego/Principal.java
@@ -1,9 +1,40 @@
 package juego;
 
-public class Principal {
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
 
+/**
+ * Clase Principal del juego.
+ * @author David A Quesada C
+ *
+ */
+public class Principal extends StateBasedGame {
+	
+    public Principal() {
+    	// Titulo de la ventana
+        super("Ice Climber");
+    }
+    
+	/**
+	 * Inicializar la lista de estados del juego
+	 */
+    public void initStatesList(GameContainer container) throws SlickException {
+
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+        try {
+            AppGameContainer app = new AppGameContainer(new Principal());
+            app.setDisplayMode(640, 480, false);
+            app.setShowFPS(false);
+            app.start();
+        } catch (SlickException slick) {
+            slick.printStackTrace();
+        }
 
 	}
 
