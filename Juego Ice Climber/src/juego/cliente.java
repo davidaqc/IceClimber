eca5ea7a1ba7e47847c410d25329418ee6cd3b68
Steven <stevencm199gmail.com>
2020-12-15 15:58:32 -0600

    Servidor basico

---
+++ Juego Ice Climber/src/juego/cliente.java
@@ -0,0 +1,49 @@
+
+package juego; 
+
+import java.net.*;
+import java.io.*;
+ 
+/**
+ * This program demonstrates a socket client program that talks to a SMTP server.
+ *
+ * @author www.codejava.net
+ */
+public class cliente {
+ 
+    public static void conectar() {
+ 
+        String hostname = "localhost";
+        int port = 8888;
+ 
+        try (Socket socket = new Socket(hostname, port)) {
+ 
+            InputStream input = socket.getInputStream();
+ 
+            OutputStream output = socket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+ 
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+ 
+            String line = reader.readLine();
+            System.out.println(line);
+ 
+            writer.println("helo " + hostname);
+ 
+            line = reader.readLine();
+            System.out.println(line);
+ 
+            writer.println("quit");
+            line = reader.readLine();
+            System.out.println(line);
+ 
+        } catch (UnknownHostException ex) {
+ 
+            System.out.println("Server not found: " + ex.getMessage());
+ 
+        } catch (IOException ex) {
+ 
+            System.out.println("I/O error: " + ex.getMessage());
+        }
+    }
+}
